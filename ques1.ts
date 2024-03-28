6dd82187a11acaac05edae7d8da55ed3e07b4efa
Dania Haroon <159708973+Daniaharoon@users.noreply.github.com>
2024-03-28 10:05:01 +0500

    Add files via upload

---
+++ ques1.ts
@@ -0,0 +1,7 @@
+// *. Install Node.js, TypeScript and VS Code on your computer.
+//1.https://code.visualstudio.com/  download for (vs code).
+// 2.https://nodejs.org/en / download for (node.js).
+// 3.npm i -g  typescript (install typescript key).
+//4. tsc --init  for(tsconfig.json key).
+//5. npm init -y for(package.json key) .
+//6. npm i @types/node -D for(package_lock.json key).
